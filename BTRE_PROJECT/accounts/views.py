e93cc90ac73fc4c732e035715e5be444745121e7
Masud Dipu <masuddipuofficial@gmail.com>
2019-07-27 02:00:03 +0600

    final commit

---
+++ BTRE_PROJECT/accounts/views.py
@@ -0,0 +1,65 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
+
+from contacts.models import Contact
+
+# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'Username not available! Choose another.')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'This email is already being used!')
+                    return redirect('register')
+                else:
+                    user = User.objects.create_user(username=username, password=password, email=email,
+                     first_name=first_name, last_name=last_name)
+                    user.save()
+                    messages.success(request, 'You are now successfully registered. Please login')
+                    return redirect('login')
+        else:
+            messages.error(request, 'Passwords do not match!')
+            return redirect('register')
+    else:
+        return render(request, 'accounts/register.html')
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user:
+            auth.login(request, user)
+            messages.success(request, 'You are now successfully logged in.')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid credentials. Wrong username or password.')
+            return redirect('login')
+    else:
+        return render(request, 'accounts/login.html')
+
+
+def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        messages.success(request, 'You are noe logged out')
+    return redirect('index')
+
+def dashboard(request):
+    contacts = Contact.objects.order_by('-contact_date').filter(user_id=request.user.id)
+    context = {
+        'contacts': contacts,
+    }
+    return render(request, 'accounts/dashboard.html', context)
---
+++ BTRE_PROJECT/pages/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from listings.models import Listing
+from realtors.models import Realtor
+
+from listings.choices import bedroom_choices, price_choices, state_choices
+# Create your views here.
+def index(request):
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+
+    context = {
+        'listings':listings,
+        'bedroom_choices':bedroom_choices,
+        'price_choices':price_choices,
+        'state_choices':state_choices,
+    }
+    return render(request, 'pages/index.html', context)
+
+def about(request):
+    realtors = Realtor.objects.order_by('-hire_date')
+    mvp_realtor = Realtor.objects.filter(is_mvp=True)
+    context = {
+        'realtors':realtors,
+        'mvp_realtor':mvp_realtor,
+    }
+    return render(request, 'pages/about.html', context)
